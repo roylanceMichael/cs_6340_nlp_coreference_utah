f183f65b3a63a86a49ce018c71100d1307a2e44f
Mike Roylance <roylance.michael@gmail.com>
2012-10-20 18:10:02 -0600

    new function: allWords

--- appcode/devset/input/crf.rb
+++ appcode/devset/input/crf.rb
@@ -7,6 +7,26 @@ class Crf
     @rawXml = REXML::Document.new(content)    
   end
   
+  def allWords
+    words = []
+    #only grab first node
+    if @rawXml != nil && @rawXml.length > 0
+      rootXml = @rawXml[0]
+      rootXml.each do |element|
+        if element.class == REXML::Element
+          element.get_text.to_s.split(/\s+/).each do |word|
+            words.push word
+          end
+        elsif element.class == REXML::Text
+          element.to_s.split(/\s+/).each do |word|
+            words.push word
+          end
+        end
+      end
+    end
+    words
+  end
+  
   def corefs
     ret = []
     if @rawXml != nil && @rawXml.length > 0
