4cfbeadba914547f50bac193fa1056b5d725c0b3
Ben Nelson <ben@bytebot.(none)>
2012-10-27 18:09:32 -0600

    add in the code for the JRuby, using java classes for the stanford parser

--- appcode/parseAdapter.rb
+++ appcode/parseAdapter.rb
@@ -1,5 +1,12 @@
-class ParseAdapter
-  attr_accessor :execPath, :tempFile
+require 'java'
+include Java
+require 'stanford-parser/stanford-parser.jar'
+include_class 'java.io.StringReader'
+include_class 'edu.stanford.nlp.parser.lexparser.LexicalizedParser'
+include_class 'edu.stanford.nlp.trees.PennTreebankLanguagePack'
+
+module ParseAdapter
+=begin  attr_accessor :execPath, :tempFile
   
   def initialize
     @execPath = "java -mx150m -cp stanford-parser/*: edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat penn,typedDependencies edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
@@ -18,4 +25,10 @@ class ParseAdapter
     result = %x(#{@execPath} #{@tempFile})
     result
   end
+=end
+    def ParseAdapter.parse(sentence)
+	lp = LexicalizedParser.getParserFromSerializedFile "englishPCFG.ser.gz"
+	parse =	lp.apply(sentence)
+	return parse.toString()	
+    end
 end
