f17387f24ebba1ea51876820c108a54e78fad615
Mike Roylance <roylance.michael@gmail.com>
2012-11-03 18:41:51 -0600

    plurality stuff for npmodel

--- appcode/npModel.rb
+++ appcode/npModel.rb
@@ -1,7 +1,9 @@
 # poro for handling info... i like strongly typed for this stuff
 class NpModel
   #ref is a reference to another NpModel...
-  attr_accessor :id, :startIdx, :endIdx, :sentIdx, :phrase, :sent, :ref, :coref
+  attr_accessor :id, :startIdx, :endIdx, :sentIdx, :phrase, :sent, :ref, 
+                :coref, :position, :pronounType, :article, :appositive, 
+                :plurality, :properName, :semanticClass, :gender, :animacy, :headNoun
   
   #sent model, 
   def initialize(id, startIdx, endIdx, phrase, sent)
@@ -10,5 +12,19 @@ class NpModel
     @endIdx = endIdx
     @phrase = phrase
     @sent = sent
+    identifyPlurality
   end
+  
+  def identifyPlurality
+    words = @phrase.split(/\s+/)
+    idx = words.length
+    wordLength = words[idx-1].length
+    if words[idx-1][wordLength-1] == 's'
+      @plurality = true
+    else
+      @plurality = false
+    end
+  end
+  
+  
 end
