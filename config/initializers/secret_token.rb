f5bde2cab3e8f1eb5346a5b26fe65321893e1377
Mike Roylance <roylance.michael@gmail.com>
2012-11-24 17:41:19 -0700

    cleaning up rails project, couldn't get it to work with jruby

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Coreference::Application.config.secret_token = '088ef79ff8c148a69632a30e50650705d97581291af33f1a2d52d386d873d20cf7632358f4cb823d7d7eb06fc80bd6906550741937816e9373aee8d9c7ac6c66'
